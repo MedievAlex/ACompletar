043b3807f65fd77f1ce8dd8d4d0f4ebb91c0b3f4
MedievAlex <alexirazola@gmail.com>
2025-11-24 11:36:23 +0100

    Fixed Flores and Color.

--- ACompletar/src/model/coleccionables/Color.java
+++ ACompletar/src/model/coleccionables/Color.java
@@ -9,28 +9,18 @@ public class Color extends Coleccionable implements Serializable {
     
     private String nombre;
     private TipoColor tipoColor;
-    private boolean obtenido;
     
     public Color(String nombre, TipoColor tipoColor) {
         super(nombre);
         this.tipoColor = tipoColor;
-        this.obtenido = false;
     }
     
     public TipoColor getTipoColor() {
         return tipoColor;
     }
 
-    public boolean isObtenido() {
-        return obtenido;
-    }
-
-    public void setObtenido(boolean obtenido) {
-        this.obtenido = obtenido;
-    }
-
     @Override
     public String toString() {
-        return "\n   |-" + super.toString() + "\n   | Tipo Color: " + tipoColor + " | Obtenido=" + obtenido + '}';
+        return "\n   |-" + super.toString() + "\n   | Tipo Color: " + tipoColor;
     }
 }
